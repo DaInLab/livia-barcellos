7c50abcb310249e18ae4809b3c69dc19837ec3c4
Lívia I Barcellos <livia.i.barcellos@unesp.br>
2022-11-26 13:44:03 -0300

    Atualizado 2022-11-26

--- codigo/Exercicio.5.R
+++ codigo/Exercicio.5.R
@@ -3,55 +3,16 @@ if (!("readxl") %in% installed.packages()) install.packages("readxl")
 
 # Carregando a biblioteca
 library(readxl)
-library(readxl)
 exercicio5 <- read_excel("dados/exercicio5.xls")
 View(exercicio5)
 
 #importar e vizualizar os dados
-exercicio4 <- read_excel("dados/exercicio4.xls", skip = 1, col_names = "salarios", col_types = c("numeric"))
-View(exercicio4)
+exercicio5 <- read_excel("dados/exercicio5.xls", skip = 1, col_names = c("marcas", "pessoas") , col_types = c("text" ,"numeric"))
 
-#Construir uma tabela de frequências
-tabela <- c(exercicio4$salarios)
-tabfreq <- table (tabela)
-tabfreq  
-#5.25  5.73  6.26  6.66  6.86  7.39  7.44  7.59  8.12  8.46  8.74 
-#1     1     1     1     1     1     1     1     1     1     1 
-#8.95  9.13  9.35  9.77   9.8 10.53 10.76 11.06 11.59 
-#1     1     1     1     1     1     1     1     1 
+View(exercicio5)
 
-
-#Opção 1:
-#salva a tabela em Tab
-Tab = table(exercicio4$salarios) 
-
-#soma os valores da tabela
-Total = sum(Tab) 
-#20L
-
-#frequência absoluta dividido pelo total é igual à frequência relativa
-Tab/Total 
-# 5.25  5.73  6.26  6.66  6.86  7.39  7.44  7.59  8.12  8.46  8.74 
-#0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05 
-#8.95  9.13  9.35  9.77   9.8 10.53 10.76 11.06 11.59 
-#0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05 
-
-barplot(tabfreq)
-print(exercicio4.table)
-
-# transformando os dados em vetor
-exercicio4[[1]] <- data.frame(exercicio4)
-class (exercicio4)
-data.frame(exercicio4)
-v.exercicio4 <- c(data.frame(exercicio4)$salarios)
-
-print (exercicio4)
-print (data.frame(exercicio4))
-
-# criando e imprimindo gráfico de histograma 
-exercicio4.table <-table(v.exercicio4) 
-
-# imprimindo a tabela de frequência
-print (exercicio4.table)
-
-
+#Construir um gráfico de barras
+exercicio5[[1]] <- data.frame(exercicio5)
+data.frame(exercicio5)
+v.exercicio5 <- c(data.frame(exercicio5)$pessoas)
+barplot(v.exercicio5)
