04eb10bd0bb35547a35f9d0ba45679a2f800bc4f
Lívia I Barcellos <livia.i.barcellos@unesp.br>
2022-12-18 00:10:05 -0300

    2022-11-17

--- codigo/Exercicio.8.R
+++ codigo/Exercicio.8.R
@@ -3,54 +3,50 @@ if (!("readxl") %in% installed.packages()) install.packages("readxl")
 
 # Carregando a biblioteca
 library(readxl)
-exercicio4 <- read_excel("dados/exercicio4.xls")
-View(exercicio4)
+exercicio8 <- read_excel("dados/exercicio8.xls")
+View(exercicio8)
 
 #importar e vizualizar os dados
-exercicio4 <- read_excel("dados/exercicio4.xls", skip = 1, col_names = "salarios", col_types = c("numeric"))
-View(exercicio4)
+exercicio8 <- read_excel("dados/exercicio8.xls", skip = 1, col_names = "altura", col_types = c("numeric"))
+View(exercicio8)
 
 #Construir uma tabela de frequências
-tabela <- c(exercicio4$salarios)
+tabela <- c(exercicio8$altura)
 tabfreq <- table (tabela)
 tabfreq  
-#5.25  5.73  6.26  6.66  6.86  7.39  7.44  7.59  8.12  8.46  8.74 
-#1     1     1     1     1     1     1     1     1     1     1 
-#8.95  9.13  9.35  9.77   9.8 10.53 10.76 11.06 11.59 
-#1     1     1     1     1     1     1     1     1 
-
-
-#Opção 1:
-#salva a tabela em Tab
-Tab = table(exercicio4$salarios) 
-
-#soma os valores da tabela
-Total = sum(Tab) 
-#20L
-
-#frequência absoluta dividido pelo total é igual à frequência relativa
-Tab/Total 
-# 5.25  5.73  6.26  6.66  6.86  7.39  7.44  7.59  8.12  8.46  8.74 
-#0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05 
-#8.95  9.13  9.35  9.77   9.8 10.53 10.76 11.06 11.59 
-#0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05  0.05 
+#1.56 1.57 1.58  1.6 1.61 1.62 1.63 1.64 1.65 1.66 1.67 1.68 1.69  1.7 1.71 1.72 1.73 1.74 1.75 1.76 
+#1    1    2    1    2    2    3    4    2    4    4    5    1    4    2    3    4    4    7    3 
+#1.77 1.78 1.79  1.8 1.81 1.82 1.83 1.86 1.87 1.89 
+#3    1    5    3    1    2    2    1    1    1 
 
 barplot(tabfreq)
-print(exercicio4.table)
+print(exercicio8.table)
 
 # transformando os dados em vetor
-exercicio4[[1]] <- data.frame(exercicio4)
-class (exercicio4)
-data.frame(exercicio4)
-v.exercicio4 <- c(data.frame(exercicio4)$salarios)
+exercicio8[[1]] <- data.frame(exercicio8)
+class (exercicio8)
+data.frame(exercicio8)
+v.exercicio8 <- c(data.frame(exercicio8)$altura)
 
-print (exercicio4)
-print (data.frame(exercicio4))
+print (exercicio8)
+print (data.frame(exercicio8))
 
 # criando e imprimindo gráfico de histograma 
-exercicio4.table <-table(v.exercicio4) 
+exercicio8.table <-table(v.exercicio8) 
 
 # imprimindo a tabela de frequência
-print (exercicio4.table)
+print (exercicio8.table)
+hist(v.exercicio8$altura)
+hist(v.exercicio8$altura, breaks = 30, col = "blue", xlab = "Alturas")
+hist(v.exercicio8$altura, breaks = 30, col = "blue", xlab = "Alturas", ylab = "Frequência", main = " Histograma de altura")
 
+#Construir uma tabela de frequências
+tabela <- c(exercicio8)
+tabfreq <- table (tabela)
+tabfreq  
+
+
+# criando e imprimindo gráfico de histograma 
+exercicio8.table <-table(exercicio8) 
+View(exercicio8.table)
 
